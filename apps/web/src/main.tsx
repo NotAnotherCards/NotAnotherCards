6b78c7b4f9504df4337a06085ee6c6395cb82829
Danielg1406 <danielgomeza1406@gmail.com>
2026-07-07 00:45:59 +0200

    feat: add Tailwind

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,7 +3,8 @@ import "./style.css";
 import typescriptLogo from "/typescript.svg";
 
 const App = () => (
-  <div>
+  <div className="flex">
+    <h1 className="text-blue-500 text-3xl font-bold underline">Hello World</h1>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" className="logo" alt="Vite logo" />
     </a>
