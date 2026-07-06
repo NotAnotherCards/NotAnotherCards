a5b5e58961a9ad6e415b50ac5d70319ddbec07cb
Danielg1406 <danielgomeza1406@gmail.com>
2026-07-07 01:05:48 +0200

    feat: initialize shadcn

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,9 +1,10 @@
 import { createRoot } from "react-dom/client";
 import "./style.css";
 import typescriptLogo from "/typescript.svg";
+import { Button } from "./components/ui/button";
 
 const App = () => (
-  <div className="flex">
+  <div className="flex gap-7">
     <h1 className="text-blue-500 text-3xl font-bold underline">Hello World</h1>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" className="logo" alt="Vite logo" />
@@ -15,6 +16,7 @@ const App = () => (
         alt="TypeScript logo"
       />
     </a>
+    <Button variant="outline">Test</Button>
   </div>
 );
 
