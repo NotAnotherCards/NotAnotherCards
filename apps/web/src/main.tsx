8e261df2f923f7f2b2ad38d5b39b7d70f78d1b3e
Danielg1406 <danielgomeza1406@gmail.com>
2026-07-08 22:59:08 +0200

    refactor: split main into main and App for easier mount logic

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,23 +1,5 @@
 import { createRoot } from "react-dom/client";
 import "./style.css";
-import typescriptLogo from "/typescript.svg";
-import { Button } from "./components/ui/button";
-
-const App = () => (
-  <div className="flex gap-7">
-    <h1 className="text-blue-500 text-3xl font-bold underline">Hello World</h1>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" className="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img
-        src={typescriptLogo}
-        className="logo vanilla"
-        alt="TypeScript logo"
-      />
-    </a>
-    <Button variant="outline">Test</Button>
-  </div>
-);
+import { App } from "./App";
 
 createRoot(document.getElementById("app")!).render(<App />);
