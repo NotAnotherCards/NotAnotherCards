d42079de40d00318730b37a47ab836205a8eba85
samsnwn <samuel.c.glantz@gmail.com>
2026-07-14 11:35:48 +0200

    feat: integrate react-hook-form and zod validation into login and register routes

--- apps/web/src/routes/_auth/login.tsx
+++ apps/web/src/routes/_auth/login.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
 import {
   Field,
+  FieldError,
   FieldGroup,
   FieldLabel,
   FieldLegend,
@@ -8,29 +9,77 @@ import {
 } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
 import { createFileRoute } from "@tanstack/react-router";
+import { Controller, useForm } from "react-hook-form";
+import z from "zod";
+import { loginSchema } from "@repo/schemas";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 export const Route = createFileRoute("/_auth/login")({
   component: LoginComponent,
 });
 
+type LoginValues = z.infer<typeof loginSchema>;
+
 function LoginComponent() {
+  const form = useForm<LoginValues>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (data: LoginValues) => {
+    console.log(data);
+  };
+
   return (
     <div>
-      <form>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
         <FieldSet>
           <FieldLegend>Welcome back!</FieldLegend>
           <FieldGroup>
-            <Field>
-              <FieldLabel htmlFor="email">Email</FieldLabel>
-              <Input id="email"/>
-            </Field>
-            <Field>
-              <FieldLabel htmlFor="password">Password</FieldLabel>
-              <Input id="password"/>
-            </Field>
+            <Controller
+              name="email"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor="email">Email</FieldLabel>
+                  <Input
+                    {...field}
+                    id="email"
+                    type="email"
+                    aria-invalid={fieldState.invalid}
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+            <Controller
+              name="password"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field>
+                  <FieldLabel htmlFor="password">Password</FieldLabel>
+                  <Input
+                    {...field}
+                    id="password"
+                    type="password"
+                    aria-invalid={fieldState.invalid}
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
           </FieldGroup>
         </FieldSet>
-        <Button type="submit" className="mt-6">Login</Button>
+        <Button type="submit" className="mt-6">
+          Login
+        </Button>
       </form>
     </div>
   );
--- apps/web/src/routes/_auth/register.tsx
+++ apps/web/src/routes/_auth/register.tsx
@@ -1,6 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
 import {
   Field,
+  FieldError,
   FieldGroup,
   FieldLabel,
   FieldLegend,
@@ -8,38 +9,111 @@ import {
 } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-
+import { Controller, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { registerSchema } from "@repo/schemas";
+import z from "zod";
 
 export const Route = createFileRoute("/_auth/register")({
   component: RegisterComponent,
 });
 
+type RegisterValues = z.infer<typeof registerSchema>;
+
 function RegisterComponent() {
+  const form = useForm<RegisterValues>({
+    resolver: zodResolver(registerSchema),
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  const onSubmit = (data: RegisterValues) => {
+    console.log(data);
+  };
+
   return (
     <div>
-      <form>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
         <FieldSet>
           <FieldLegend>Create Account</FieldLegend>
           <FieldGroup>
-            <Field>
-              <FieldLabel htmlFor="name">Name</FieldLabel>
-              <Input id="name"/>
-            </Field>
-            <Field>
-              <FieldLabel htmlFor="email">Email</FieldLabel>
-              <Input id="email"/>
-            </Field>
-            <Field>
-              <FieldLabel htmlFor="password">Password</FieldLabel>
-              <Input id="password"/>
-            </Field>
-            <Field>
-              <FieldLabel htmlFor="confirmPassword">Confirm Password</FieldLabel>
-              <Input id="confirmPassword"/>
-            </Field>
+            <Controller
+              name="firstName"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field>
+                  <FieldLabel htmlFor="firstName">First Name</FieldLabel>
+                  <Input {...field} id="firstName" />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />{" "}
+            <Controller
+              name="lastName"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field>
+                  <FieldLabel htmlFor="lastName">Last Name</FieldLabel>
+                  <Input {...field} id="lastName" />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />{" "}
+            <Controller
+              name="email"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field>
+                  <FieldLabel htmlFor="email">Email</FieldLabel>
+                  <Input {...field} id="email" />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+            <Controller
+              name="password"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field>
+                  <FieldLabel htmlFor="password">Password</FieldLabel>
+                  <Input {...field} id="password" type="password" />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />{" "}
+            <Controller
+              name="confirmPassword"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field>
+                  <FieldLabel htmlFor="confirmPassword">
+                    Confirm Password
+                  </FieldLabel>
+                  <Input {...field} id="confirmPassword" type="password" />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
           </FieldGroup>
         </FieldSet>
-        <Button type="submit" className="mt-6">Sign up</Button>
+        <Button type="submit" className="mt-6">
+          Sign up
+        </Button>
       </form>
     </div>
   );
