7969e0201cf04178ca3cd367c074123b00505a39
Danielg1406 <danielgomeza1406@gmail.com>
2026-07-23 18:42:54 +0200

    fix: delete invalid migration files from last PR

--- apps/api/drizzle/0001_new_anthem.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "user" ALTER COLUMN "name" DROP NOT NULL;--> statement-breakpoint
-ALTER TABLE "user" ADD COLUMN "first_name" text NOT NULL;--> statement-breakpoint
-ALTER TABLE "user" ADD COLUMN "last_name" text NOT NULL;
