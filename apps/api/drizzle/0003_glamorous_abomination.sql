4d1d223e161b851298e87d020cfac8116d70df9f
Tanmay <tanmaypandya159@gmail.com>
2026-07-28 19:06:37 +0200

    fix(db): update 'updated_at' colomn to be initialized with 'created_at' value in case it is null

--- apps/api/drizzle/0003_glamorous_abomination.sql
+++ apps/api/drizzle/0003_glamorous_abomination.sql
@@ -1,2 +1,3 @@
 ALTER TABLE "user_decks" ALTER COLUMN "updated_at" SET DEFAULT now();--> statement-breakpoint
+UPDATE "user_decks" SET "updated_at" = "created_at" WHERE "updated_at" IS NULL;
 ALTER TABLE "user_decks" ALTER COLUMN "updated_at" SET NOT NULL;
