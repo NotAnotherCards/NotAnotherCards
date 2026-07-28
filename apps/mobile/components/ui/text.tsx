a67437fdceaf70489c9b90c75c5ea55389c13b5b
Dusty Way <dustyway@mailbox.org>
2026-07-28 22:29:36 +0200

    chore: pin mobile prettier config to existing code style

--- apps/mobile/components/ui/text.tsx
+++ apps/mobile/components/ui/text.tsx
@@ -4,5 +4,7 @@ import { cn } from '../../lib/utils'
 type TextProps = React.ComponentProps<typeof RNText>
 
 export function Text({ className, ...props }: TextProps) {
-  return <RNText className={cn('text-base text-zinc-900', className)} {...props} />
+  return (
+    <RNText className={cn('text-base text-zinc-900', className)} {...props} />
+  )
 }
