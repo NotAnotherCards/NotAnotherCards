865c6e73a9495408f19ab7ec7812d89bd5201ac0
Danielg1406 <danielgomeza1406@gmail.com>
2026-09-14 21:11:47 +0200

    feat: disable no-useless-escape rule to accommodate regex semantics

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -15,6 +15,9 @@ module.exports = {
   },
   rules: {
     '@typescript-eslint/no-non-null-assertion': 'off',
+    // The core rule does not account for regex semantics; use the
+    // regexp-aware replacement below for regular expression literals.
+    'no-useless-escape': 'off',
     // Keep character-class edits from silently widening security regexes.
     'regexp/no-obscure-range': 'error',
     'regexp/no-useless-escape': 'error',
