bf37702ccad9ba373501ab28621fe306c8489de8
samsnwn <samuel.c.glantz@gmail.com>
2026-09-23 20:53:56 +0200

    feat: wrap root App component in Suspense boundary

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,9 +2,14 @@ import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import './style.css';
 import './lib/i18n';
+import { Suspense } from 'react';
 const rootElement = document.getElementById('app')!;
 
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
-  root.render(<App />);
+  root.render(
+    <Suspense fallback={null}>
+      <App />
+    </Suspense>
+);
 }
